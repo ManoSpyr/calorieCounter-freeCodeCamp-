6701acdb4d13771bcad507b162ddfed394fffc34
ManoSpyr <m.spyropoulos@yahoo.com>
2024-03-07 18:34:07 +0200

    allow users to add entries to the calorie counter.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
+
 let isError = false;
 function cleanInputString(str) {
   /* const strArray = str.split('');
@@ -17,5 +18,12 @@ function cleanInputString(str) {
   return str.replace(regex, "");
 }
 function isInvalidInput(str) {
-  const regex = /e/i;
+  const regex = /+\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
 }
