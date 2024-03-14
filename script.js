d55141dc7a6459da55c8e675a461b5f5c2a0a276
ManoSpyr <m.spyropoulos@yahoo.com>
2024-03-14 15:57:02 +0200

    see  new HTML content

--- script.js
+++ script.js
@@ -29,8 +29,9 @@ function addEntry() {
   const entryNumber =
     targetInputContainer.querySelectorAll('input[type="text"]').length; // get all of the number inputs
   var HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>;
-  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name">
-  <label></label>
-  `;
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"}/>`;
+  targetInputContainer.innerHTML += HTMLString;
 }
