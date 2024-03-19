e28dab1d29a5d64fd07b4b7e2989c08ba71e4466
ManoSpyr <m.spyropoulos@yahoo.com>
2024-03-19 16:43:05 +0200

    add entries without losing your previous inputs.

--- script.js
+++ script.js
@@ -4,34 +4,32 @@ const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
-
 let isError = false;
+
 function cleanInputString(str) {
-  /* const strArray = str.split('');
-    const cleanStrArray = [];
-    for (let i = 0; i < strArray.length; i++) {
-        if (!["+", "-", " "].includes(strArray[i])) {
-            cleanStrArray.push(strArray[i]);
-        };
-    }*/
-  const regex = /[+-\s]/g; //s matching any whitespace character[]=>to o match each of these characters individually
+  const regex = /[+-\s]/g;
   return str.replace(regex, "");
 }
+
 function isInvalidInput(str) {
-  const regex = /+\d+e\d+/i;
+  const regex = /\d+e\d+/i;
   return str.match(regex);
 }
-
 function addEntry() {
   const targetInputContainer = document.querySelector(
     `#${entryDropdown.value} .input-container`
   );
   const entryNumber =
-    targetInputContainer.querySelectorAll('input[type="text"]').length; // get all of the number inputs
-  var HTMLString = `
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"}/>`;
-  targetInputContainer.innerHTML += HTMLString;
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
