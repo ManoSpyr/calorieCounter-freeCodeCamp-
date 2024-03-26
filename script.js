cbe322c684631c980c2e4c2a1af7ee7322244943
ManoSpyr <m.spyropoulos@yahoo.com>
2024-03-26 13:11:40 +0200

    check  invalidInputMatch, message for invalid and return null(Curval to number)

--- script.js
+++ script.js
@@ -39,5 +39,12 @@ function getCaloriesFromInputs(list) {
     console.log(calories);
     const currVal = cleanInputString(item.value);
     let invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
   }
 }
